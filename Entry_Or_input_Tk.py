784329db2cff53eef60ccf6dea3b5a7cc561b6dc
SR2050 <shiboshreeroy0112gmail.com>
2025-05-08 16:13:02 +0600

    Add Functional input box  Example With tkinter

--- Entry_Or_input_Tk.py
+++ Entry_Or_input_Tk.py
@@ -6,10 +6,20 @@ root.title("Entry Example With Tkinter")
 root.geometry("300x200")
 
 
+def GreetUser():
+  name = entry.get() # Get the name entered by the user
+  greeting = "Hello, " + name # Create a greeting message
+  label = tk.Label(root, text=greeting, bg="blue", fg="white") # Create a label to display the greeting message
+  label.pack(pady=20) # Pack the label into the window
+
 # Create an Entry
 name = tk.StringVar() # Variable to store the name entered by the user
 label = tk.Label(root,text  = "Enter Your Name:", bg="blue",fg="white")
 label.pack(pady=20)
 entry =tk.Entry(root,textvariable=name)
 entry.pack(pady=20)
+
+button = tk.Button(root,text="Greet Me!",bg="blue",fg="white", command=GreetUser)
+button.pack(pady=20)
+
 root.mainloop()
