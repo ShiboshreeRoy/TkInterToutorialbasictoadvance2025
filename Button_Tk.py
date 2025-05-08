0a334b6a3c147cdab28e121be38d67e7dd3c2afa
SR2050 <shiboshreeroy0112gmail.com>
2025-05-08 15:55:12 +0600

    Add Functional  Button Example With tkinter

--- Button_Tk.py
+++ Button_Tk.py
@@ -5,9 +5,18 @@ import tkinter as tk
 root =tk.Tk()
 root.title("Button Example With Tkinter")
 root.geometry("400x400")
+
+# Create a label to display the button click message
+def on_button_click():
+    label_result =tk.Label(root,text="Button Clicked!")
+    label_result.pack(pady=20)
+
+
 # Create a button
 
-buton = tk.Button(root,text="ClickMe!",bg="blue",fg="white")
+buton = tk.Button(root,text="ClickMe!",bg="blue",fg="white", command=on_button_click)
 buton.pack(pady=20)
+
+
 
 root.mainloop()
